8b3a18e173a370d1faf2086d067c39291d7d4498
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2026-08-24 20:00:09 +0300

    feat: initialize application structure and database connection configuration

---
+++ includes/app.php
@@ -0,0 +1,17 @@
+<?php
+
+$database_info = include __DIR__ . './../config/database.php';
+
+try {
+
+    $connect = mysqli_connect(
+        $database_info['host'],
+        $database_info['user'],
+        $database_info['password'],
+        $database_info['database']
+    );
+
+
+} catch (Exception $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
